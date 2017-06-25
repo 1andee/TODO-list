5defb57ee0238a307374491de0391a328aff75a4
kamir222 <katia.amir@gmail.com>
2017-06-25 22:33:10 +0000

    remove ajax request to delete item from display, not db

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -432,19 +432,12 @@ $(() => {
       } else {
         $(this).text('Done');
 
-        // ajax post request to delete item
-        //remove item after 750 milliseconds
+        //remove item from display after 750 milliseconds
         setTimeout(() => {
-          $.ajax({
-            method: 'POST',
-            url: '/list/delete',
-            data: item_id
-          }).then(()=>{
-            $(this).closest("article").remove();
-          })
+          $(this).closest("article").remove();
         }, 750);
-      }
 
+      }
 
     });
 
