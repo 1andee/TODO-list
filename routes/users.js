7c008d979739d0552cc282b72248476a29d20208
Ian Duke <ian.c.duke@gmail.com>
2017-08-03 18:27:02 -0400

    Revise knex query

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ module.exports = (knex) => {
   // make items table availabe at /list
   router.get("/list", (req, res) => {
     knex('items')
-      .where({ user_id: req.session.user_id })
+      .where('user_id': req.session.user_id)
       .then((results) => {
         res.json(results);
     });
